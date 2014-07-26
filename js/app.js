2719a9e8c0689562deb465d0d6553bee8860fa05
tag1 <rlthompsonjr@gmail.com>
2014-07-26 05:04:50 -0400

    draft 1
    added number generation, added new game button functionality. Started
    guess button.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 
 $(document).ready(function(){
 	
+	/* Secret Number generation */
+	var numGeneration = function() {
+	var secretNumber = Math.floor(Math.random() * 101);
+	console.log(secretNumber);
+	};
+
+
+	/* New Game button */
+	$(".new").click(function () {
+		$("#userGuess").val("");
+		numGeneration();
+	});
+
+	/* Guess Feedback function */
+	$("#guessButton").click(function() {
+		
+	}
+
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
