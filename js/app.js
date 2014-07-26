182f5b767c26d5e5926024e88f166b7d9b81f35e
tag1 <rlthompsonjr@gmail.com>
2014-07-26 06:10:37 -0400

    draft 2
    added major functionality to guess button, added guess list, added
    counter to guess count.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,47 @@
 
 $(document).ready(function(){
 	
+
 	/* Secret Number generation */
-	var numGeneration = function() {
 	var secretNumber = Math.floor(Math.random() * 101);
 	console.log(secretNumber);
-	};
 
+	/* Guess count variable */
+	var countNumber = $("#count").val();
+	var guessCount = +countNumber;
+	console.log("Count is at " + guessCount)
+
+
+	/* Guess Feedback function */
+	var buttonClick = $("#guessButton").click(function() {
+		var guess = $("#userGuess").val();
+		if (guess == secretNumber) {
+			$("#feedback").replaceWith( "<h2 id='feedback'>" + "You win!" + "</h2>");
+			guessCount++;
+			$("#count").replaceWith("<span id='count'>" + guessCount + "</span>");
+			$("#guessList").prepend("<li style='display:block;'>" + "Victory! " + guess + " is the correct answer!" + "</li>");
+			} else if 
+				(guess > secretNumber) {
+				 $("#feedback").replaceWith("<h2 id='feedback'>" + "You are too high!" + "</h2>");
+				 guessCount++;
+				 $("#count").replaceWith("<span id='count'>" + guessCount + "</span>");
+				 $("#guessList").append("<li>" + guess + "</li>");
+				}
+					else if (guess < secretNumber) {
+					 $("#feedback").replaceWith("<h2 id='feedback'>" + "You are too low!" + "</h2>");
+					 guessCount++;
+					 $("#count").replaceWith("<span id='count'>" + guessCount + "</span>");
+					 $("#guessList").append("<li>" + guess + "</li>");
+					};
+	});
 
 	/* New Game button */
 	$(".new").click(function () {
 		$("#userGuess").val("");
-		numGeneration();
+		var secretNumber = Math.floor(Math.random() * 101);
+		console.log(secretNumber);
+		$("count").replaceWith("0");	
 	});
-
-	/* Guess Feedback function */
-	$("#guessButton").click(function() {
-		
-	}
-
-
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
